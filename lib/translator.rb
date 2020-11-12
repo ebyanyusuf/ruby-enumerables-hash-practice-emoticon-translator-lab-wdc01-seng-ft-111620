8b628b3a0d0b3b8007e0b63e5c1045eb8c25505a
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-12 21:04:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(emoticons,emoticon)
 end
 
 def get_english_meaning(emoticons, emoticon)
-  YAML.load_file(emoticons).each do |key, value|
+  load_library(emoticons).each do |key, value|
     if value[:japanese] == emoticon
       return key
     end
